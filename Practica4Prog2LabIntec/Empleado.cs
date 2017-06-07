c9459adc96c3898e9b6f7b53a6a1eef311f0a0f7
Gabriel Paredes <paredesgabrielb@gmail.com>
2017-06-06 20:15:32 -0400

    Practica 4 First Commit

---
+++ Practica4Prog2LabIntec/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4Prog2LabIntec
+{
+    class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public double Salario { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IdEmpleado}|{Nombre}|{Salario}";
+        }
+    }
+}
---
+++ Practica4Prog2LabIntec/Ponche.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4Prog2LabIntec
+{
+    class Ponche
+    {
+        public int IdEmpleado { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Tipo Tipo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IdEmpleado}|{Fecha:YYYY/MM/DD HH.MM}|{Tipo}";
+        }
+    }
+
+    public enum Tipo { Entrada, Salida };
+
+
+}
